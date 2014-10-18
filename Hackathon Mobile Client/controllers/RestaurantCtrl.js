a6adf201fe9fc09938140c4c80c6f4b37bc14839
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-10-18 14:27:04 +0300

    Fixed failed commit

--- Hackathon Mobile Client/controllers/RestaurantCtrl.js
+++ Hackathon Mobile Client/controllers/RestaurantCtrl.js
@@ -1,29 +1,26 @@
 'use strict';
 
-app.controller('RestaurantController', ['$scope', 'RestaurantServices', function ($scope, RestaurantServices) {
-    $scope.retrieveRestaurants = retrieveRestaurants;
-    $scope.retrieveImage = retrieveImage;
+app.controller('RestaurantController', [
+                   '$scope', 'RestaurantServices', function ($scope, RestaurantServices) {
+                       $scope.retrieveRestaurants = retrieveRestaurants;
+                       $scope.retrieveImage = retrieveImage;
 
-    function retrieveRestaurants() {
-        RestaurantServices.getRestaurants()
-            .then(function (success) {
-                
-                $scope.data = success.Result;
-                //console.log(success.Resuclt.length);
-            }, function (error) {
-                console.log(error);
-            })
-    }
+                       function retrieveRestaurants() {
+                           RestaurantServices.getRestaurants()
+                               .then(function (success) {
+                                   $scope.data = success.Result;
+                               }, function (error) {
+                                   console.log(error);
+                               })
+                       }
 
-    function retrieveImage(restaurant) {
-        console.log(restaurant);
-        console.log(restaurant.Image);
-        //RestaurantServices.getImage('http://api.everlive.com/v1/ISDTe40ezNnnMAmk/Files/' + restaurant.Image)
-        //    .then(function (success) {
-        //        console.log(success);
-        //        //return success.Uri;
-        //    }, function (error) {
-        //        console.log(error);
-        //    })
-    }
-}]);
+                       function retrieveImage(restaurant) {
+                           RestaurantServices.getImage('http://api.everlive.com/v1/ISDTe40ezNnnMAmk/Files/' + restaurant.Image)
+                               .then(function (success) {
+                                   $scope.imageUrl = success.Result.Uri;
+                               }, function (error) {
+                                   console.log(error);
+                               })
+                       }
+                   }
+]);
